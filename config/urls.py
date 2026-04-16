4408b70c5fd675961df852c6985df9afe6c1bdcd
reuben <56770691+reuben-good@users.noreply.github.com>
2026-04-16 10:21:48 +0000

    Containerise app

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
@@ -22,3 +24,6 @@ urlpatterns = [
     path('auth/', include('apps.neue_accounts.urls')),
     path('', include('apps.journal.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
